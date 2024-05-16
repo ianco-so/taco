c1fc0a9c4f1aaf3fcb85caf9da5c2e5294a7ec6e
ianco <ianco.so@gmail.com>
2024-05-16 17:23:46 -0300

    :bricks: ci: começo da construção da view de TacoDesign
    Além da adição do Controller, Test e View, houve eu fiz
    modificações no controller no comparativo com o livro.

---
+++ src/test/java/me/taco/controller/DesignTacoControllerTest.java
@@ -0,0 +1,27 @@
+package me.taco.controller;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(DesignTacoController.class)
+public class DesignTacoControllerTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testShowDesignForm() throws Exception {
+        this.mockMvc.perform(get("/design"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("design"))
+            .andExpect(content().string(containsString("Design your taco!")));
+    }
+}
