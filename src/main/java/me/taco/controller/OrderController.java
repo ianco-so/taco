dbebbf7d0ae66fc55c8402a17503d15448774faf
ianco <ianco.so@gmail.com>
2024-05-17 09:56:46 -0300

    :test_tube: test: Refinando os testes
    Agora o teste de processOrder do OrderController é mais bem
    pois passa um objeto TacoOrder válido e preenchido

--- src/main/java/me/taco/controller/OrderController.java
+++ src/main/java/me/taco/controller/OrderController.java
@@ -3,18 +3,17 @@ package me.taco.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
-import lombok.extern.slf4j.Slf4j;
 import me.taco.model.TacoOrder;
 
 @Controller
 @RequestMapping(path = "/orders")
 @SessionAttributes("tacoOrder")
-@Slf4j
 public class OrderController {
 
     @GetMapping("/current")
@@ -23,8 +22,7 @@ public class OrderController {
     }
 
     @PostMapping()
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
-        log.info("Order submitted: {} ", order);
+    public String processOrder(@RequestBody TacoOrder order, SessionStatus sessionStatus) {
         sessionStatus.setComplete();
         return "redirect:/";
     }
--- src/test/java/me/taco/controller/OrderControllerTest.java
+++ src/test/java/me/taco/controller/OrderControllerTest.java
@@ -1,20 +1,26 @@
 package me.taco.controller;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import me.taco.model.Ingredient;
+import me.taco.model.Taco;
 import me.taco.model.TacoOrder;
 
 
@@ -32,6 +38,17 @@ public class OrderControllerTest {
     @BeforeEach
     public void setup() {
         this.tacoOrder = new TacoOrder();
+
+        Taco taco = new Taco();
+        taco.setName("Taco 1");
+
+        Ingredient ingredient1 = new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP);
+        Ingredient ingredient2 = new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN);
+        Ingredient ingredient3 = new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE);
+
+        taco.setIngredients(List.of(ingredient1, ingredient2, ingredient3));
+
+        this.tacoOrder.addTaco(taco);
     }
 
     @Test
@@ -49,14 +66,30 @@ public class OrderControllerTest {
     @Test
     public void testProcessOrder() throws Exception {
         // ARRANGE
+        this.tacoOrder.setClientName("John Doe");
+        this.tacoOrder.setStreet("1234 Endless Street");
+        this.tacoOrder.setCity("Springfield");
+        this.tacoOrder.setState("RN");
+        this.tacoOrder.setZip("12345-678");
+        this.tacoOrder.setCcNumber("1234567890123456");
+        this.tacoOrder.setCcCVV("123");
+        this.tacoOrder.setCcExpiration("01/23");
+
+
         String tacoOrderJson = this.objectMapper.writeValueAsString(this.tacoOrder);
+
         // ACT & ASSERT
-        this.mockMvc
+        var result = this.mockMvc
             .perform(
                 post("/orders")
-                .contentType("application/json")
+                .contentType(MediaType.APPLICATION_JSON)
                 .content(tacoOrderJson)
             )
-            .andExpect(status().is3xxRedirection());
+            .andExpect(status().is3xxRedirection())
+            .andReturn();
+
+        var redirectUrl = result.getResponse().getRedirectedUrl();
+
+        assertEquals("/", redirectUrl);
     }
 }
